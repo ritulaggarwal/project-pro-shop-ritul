60583da8a19f7fb3f7c742cfdc228ff90367229b
Ritul Aggarwal <ritul.aggarwal2000@gmail.com>
2021-05-31 01:26:30 +0530

    Shipping Screen and Save Address

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,7 +25,6 @@ const authUser = asyncHandler(async (req, res) => {
 })
 
 
-
 // @desc    Register a new User
 // @route   POST /api/users
 // @access  Public
@@ -67,21 +66,20 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id)
-  
-    if (user) {
-      res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-      })
-    } else {
-      res.status(404)
-      throw new Error('User not found')
-    }
-   
-  })
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 
 
 
@@ -91,14 +89,14 @@ const getUserProfile = asyncHandler(async (req, res) => {
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
-  if(user){
-
-    user.name=req.body.name || user.name
-    user.email=req.body.email || user.email
-    if(req.body.password){
-      user.password=req.body.password
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
     }
-    const updatedUser= await user.save()
+
+    const updatedUser = await user.save()
 
     res.json({
       _id: updatedUser._id,
@@ -107,13 +105,10 @@ const updateUserProfile = asyncHandler(async (req, res) => {
       isAdmin: updatedUser.isAdmin,
       token: generateToken(updatedUser._id),
     })
-
-
-  }else {
+  } else {
     res.status(404)
     throw new Error('User not found')
   }
- 
 })
 
 
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,10 +1,11 @@
 import {
     CART_ADD_ITEM,
-    CART_REMOVE_ITEM
+    CART_REMOVE_ITEM,
+    CART_SAVE_SHIPPING_ADDRESS
   } from '../constants/cartConstants'
   
   export const cartReducer = (
-    state = { cartItems: [] },
+    state = { cartItems: [], shippingAddress: {} },
     action
   ) => {
     switch (action.type) {
@@ -28,10 +29,16 @@ import {
         }
 
         case CART_REMOVE_ITEM:
-            return {
+          return {
                 ...state,
                 cartItems: state.cartItems.filter((x)=>x.product!== action.payload)
-            }
+          }
+
+        case CART_SAVE_SHIPPING_ADDRESS:
+          return{
+            ...state,
+            shippingAddress: action.payload
+          }
 
         default:
             return state
