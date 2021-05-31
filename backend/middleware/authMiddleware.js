f019926e76a026e46fd05c090949787f0fc394ae
Ritul Aggarwal <ritul.aggarwal2000@gmail.com>
2021-06-01 03:31:24 +0530

    Admin Middleware and Get Users Endpoint

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,12 +9,11 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
-       // console.log(req.headers.authorization)
     try {
       token = req.headers.authorization.split(' ')[1]
-      
+
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      console.log(decoded)
+
       req.user = await User.findById(decoded.id).select('-password')
 
       next()
@@ -31,6 +30,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 })
 
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorized as an admin')
+  }
+}
 
- 
-export { protect }
+export { protect, admin }
